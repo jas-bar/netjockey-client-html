c7edbb4cc1fd031f6affdcb60eba81b970c34844
Dušan <dbaran@hotmail.sk>
2016-01-14 22:07:04 +0100

    Added multiple views, YT player uses library for angular

--- app/app.js
+++ app/app.js
@@ -1,56 +1,94 @@
-app = angular.module("DUDJ", []);
+app = angular.module("DUDJ", ['ngRoute', 'youtube-embed']);
+
+app.config(function($routeProvider){
+  $routeProvider.when('/', {
+    templateUrl: 'app/views/home.html'
+  });
+  $routeProvider.when('/room/:roomID', {
+    templateUrl: 'app/views/room.html',
+    controller: 'RoomController'
+  });
+  $routeProvider.otherwise({
+    redirectTo: '/'
+  });
+});
 
 app.run(function($rootScope, $window, $http){
   $http.defaults.headers.post["Content-Type"] = "application/json";
-  $rootScope.vidoeUUID = 0;
-  $rootScope.playlist = [];
-  $window.onYouTubeIframeAPIReady = function(){
-    $rootScope.player = new YT.Player('player', {
-      showinfo: 0,
-      disablekb: 0
-    });
-  };
 });
 
-app.controller('PlayerController', function($rootScope, $scope, $interval){
+app.controller('RoomController', function($rootScope, $scope, $routeParams, $http){
+  $rootScope.playlist = [];
+  $rootScope.videoUUID = 0;
+  $rootScope.roomID = $routeParams.roomID;
+  $http({
+    url: 'http://192.168.0.15:9000/v1/room/'+$rootScope.roomID,
+    method: 'GET'
+  }).then(function(response){
+    $rootScope.room = response.data.roomInfo;
+    $rootScope.startSeconds = response.data.currentSongTime;
+    $rootScope.playlist = response.data.queue.playlist;
+    $rootScope.playerVars = {
+      origin: document.domain,
+      start: $rootScope.startSeconds,
+      enablejsapi: 1,
+      controls: 0
+    };
+    $rootScope.videoID = $rootScope.playlist[0].id;
+    $scope.videoID = $rootScope.videoID;
+  });
+  console.log("RoomController: "+$rootScope.videoID+" "+$rootScope.startSeconds);
+});
+
+app.controller('PlayerController', function($rootScope, $interval, $scope){
   $scope.$on('youtube.player.ready', function ($event, player) {
-    $rootScope.player = player;
+    player.seekTo($rootScope.startSeconds, false);
     player.playVideo();
   });
+  $scope.$on('youtube.player.ended', function ($event, player) {
+    //player.loadVideoById($scope.videoID, $scope.startSeconds);
+  });
   $interval(function(){
-    console.log("[DEBUG]: Refreshing player");
     if(($rootScope.playlist[0]) && ($rootScope.playlist[0].uuid != $rootScope.videoUUID)) {
       console.log("[DEBUG]: Changing song");
-      $rootScope.player.loadVideoById($rootScope.playlist[0].id, $rootScope.startSeconds);
       $rootScope.videoUUID = $rootScope.playlist[0].uuid;
+      $rootScope.playerVars = {
+        origin: document.domain,
+        start: $rootScope.startSeconds,
+        enablejsapi: 1,
+        controls: 0
+      };
+      $rootScope.videoID = $rootScope.playlist[0].id;
+      $scope.videoID = '';
+      $scope.videoID = $rootScope.videoID;
     }
-  }, 3000);
+    console.log("PlayerController: "+$rootScope.videoID+" "+$rootScope.startSeconds);
+  }, 1000);
 });
 
-app.controller('PlaylistController', function($http, $rootScope, $scope, $interval){
+app.controller('PlaylistController', function($rootScope, $http, $scope, $interval){
   $interval(function(){
-    console.log("[DEBUG]: Refreshing playlist");
     $http({
       method: 'GET',
-      url: 'http://192.168.0.15:9000/v1/room/a/queue'
+      url: 'http://192.168.0.15:9000/v1/room/'+$scope.roomID+'/queue'
     }).then(function(response){
       $rootScope.playlist = response.data.playlist;
-      console.log($rootScope.playlist);
     });
     $http({
       method: 'GET',
-      url: 'http://192.168.0.15:9000/v1/room/a/time'
+      url: 'http://192.168.0.15:9000/v1/room/'+$scope.roomID+'/time'
     }).then(function(response){
       $rootScope.startSeconds = response.data.currentSongTime;
     });
-  }, 3000);
+    console.log("PlaylistController: "+$rootScope.videoID+" "+$rootScope.startSeconds);
+  }, 1000);
 });
 
-app.controller('SubmitSongController', function($http, $scope){
+app.controller('SubmitSongController', function($rootScope, $http, $scope){
   $scope.submitSong = function(){
-    console.log("[DEBUG]: Submitting song: " + $scope.songURL);
+    console.log("[DEBUG]: Submitting song: " + $rootScope.songURL);
     $http({
-      url: 'http://192.168.0.15:9000/v1/room/a/add-song',
+      url: 'http://192.168.0.15:9000/v1/room/'+$rootScope.roomID+'/add-song',
       method: 'POST',
       data: JSON.stringify({url: $scope.songURL})
     });
