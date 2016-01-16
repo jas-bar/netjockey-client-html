f502b32fb38eeb0f0581e39b53b22451fe5dc5d4
Dušan <dbaran@hotmail.sk>
2016-01-16 14:38:12 +0100

    Added fixed background image, and set some background to transparent

--- app/app.js
+++ app/app.js
@@ -2,11 +2,13 @@ var app = angular.module("DUDJ", ['ngRoute']);
 
 app.config(function($routeProvider){
   $routeProvider.when('/', {
-    templateUrl: 'app/views/home.html'
+    templateUrl: 'app/views/home.html',
+    css: 'css/home.css'
   });
   $routeProvider.when('/room/:roomID', {
     templateUrl: 'app/views/room.html',
-    controller: 'RoomController'
+    controller: 'RoomController',
+    css: 'css/room.css'
   });
   $routeProvider.otherwise({
     redirectTo: '/'
