ee88543dc89302daeaea172a811c8924f6f87855
Dušan <dbaran@hotmail.sk>
2016-01-16 15:20:59 +0100

    Change layout of home view also you can now press dudj header to go back to home view

--- app/app.js
+++ app/app.js
@@ -15,7 +15,16 @@ app.config(function($routeProvider){
   });
 });
 
+dudjMOTDS = ["Uz bez tresy.. :(",
+"Dizajner by sa zisiel!",
+"Dudududududu dudududu...",
+"Vyrobene 100% na slove.... v cesku!",
+"Chat spam ka... aha chat tu neni",
+"Niekdo by povedal ze sa tento nadpis stale meni"];
+
 app.run(function($http, $rootScope){
   $http.defaults.headers.post["Content-Type"] = "application/json";
   $rootScope.serverURL = "http://dudj-jasbar.rhcloud.com";
+  $rootScope.dudjHeader = "DUDJ pre-alpha";
+  $rootScope.dudjMOTD = dudjMOTDS[Math.floor(Math.random() * dudjMOTDS.length)];
 });
