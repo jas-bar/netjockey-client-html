43b871d6fa8d6b5abfb65a44ffa20833a2c7cd19
Dušan <dbaran@hotmail.sk>
2016-01-16 21:38:43 +0100

    Added new motds, cleaned up a code a little bit

--- app/app.js
+++ app/app.js
@@ -2,13 +2,11 @@ var app = angular.module("DUDJ", ['ngRoute']);
 
 app.config(function($routeProvider){
   $routeProvider.when('/', {
-    templateUrl: 'app/views/home.html',
-    css: 'css/home.css'
+    templateUrl: 'app/views/home.html'
   });
   $routeProvider.when('/room/:roomID', {
     templateUrl: 'app/views/room.html',
-    controller: 'RoomController',
-    css: 'css/room.css'
+    controller: 'RoomController'
   });
   $routeProvider.otherwise({
     redirectTo: '/'
@@ -20,7 +18,10 @@ dudjMOTDS = ["Uz bez tresy.. :(",
 "Dudududududu dudududu...",
 "Vyrobene 100% na slove.... v cesku!",
 "Chat spam ka... aha chat tu neni",
-"Niekdo by povedal ze sa tento nadpis stale meni"];
+"Niekdo by povedal ze sa tento nadpis stale meni",
+"Credits: CEO - Dusan Baran, Programmer - Dusan Baran, Designer - Dusan Baran, ... no a Jasek",
+"¯\\_(ツ)_/¯",
+"ヽ༼ຈل͜ຈ༽ﾉＳＬＡＭ ＹＯＵＲ ＪＡＭヽ༼ຈل͜ຈ༽ﾉ"];
 
 app.run(function($http, $rootScope){
   $http.defaults.headers.post["Content-Type"] = "application/json";
