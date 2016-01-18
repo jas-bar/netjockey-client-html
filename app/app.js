89baba376fe88c8d5f93e5e1d2288a876769715d
Dušan <dbaran@hotmail.sk>
2016-01-18 02:22:57 +0100

    Fixed bug with MOTDS

--- app/app.js
+++ app/app.js
@@ -23,13 +23,13 @@ dudjMOTDS = ["Uz bez tresky.. :(",
 "¯\\_(ツ)_/¯",
 "ヽ༼ຈل͜ຈ༽ﾉＳＬＡＭ ＹＯＵＲ ＪＡＭヽ༼ຈل͜ຈ༽ﾉ",
 "Nehladajte dudj na urban dictionary...",
-"IGN would rate 9/11",]
+"IGN would rate 9/11",
 "Gr8 b8 m8 cant even h8 so I r8 8 outta 8",
 "The right version",
 "Billy & Van approved",
 "We must construct additional rooms",
 "All your music are belong to us",
-"10 hour version";
+"10 hour version"];
 
 app.run(function($http, $rootScope){
   $http.defaults.headers.post["Content-Type"] = "application/json";
