f021af1e6a5225bbce4e65e99b4cfd01eef284f1
Dušan <dbaran@hotmail.sk>
2016-01-18 02:07:16 +0100

    I cleaned up some files and added new MOTDS

--- app/app.js
+++ app/app.js
@@ -17,11 +17,19 @@ dudjMOTDS = ["Uz bez tresky.. :(",
 "Dizajner by sa zisiel!",
 "Dudududududu dudududu...",
 "Vyrobene 100% na slove.... v cesku!",
-"Chat spam ka... aha chat tu neni",
+"Chat spam kap... aha chat tu neni",
 "Niekdo by povedal ze sa tento nadpis stale meni",
 "Credits: CEO - Dusan Baran, Programmer - Dusan Baran, Designer - Dusan Baran, ... no a Jasek",
 "¯\\_(ツ)_/¯",
-"ヽ༼ຈل͜ຈ༽ﾉＳＬＡＭ ＹＯＵＲ ＪＡＭヽ༼ຈل͜ຈ༽ﾉ"];
+"ヽ༼ຈل͜ຈ༽ﾉＳＬＡＭ ＹＯＵＲ ＪＡＭヽ༼ຈل͜ຈ༽ﾉ",
+"Nehladajte dudj na urban dictionary...",
+"IGN would rate 9/11",]
+"Gr8 b8 m8 cant even h8 so I r8 8 outta 8",
+"The right version",
+"Billy & Van approved",
+"We must construct additional rooms",
+"All your music are belong to us",
+"10 hour version";
 
 app.run(function($http, $rootScope){
   $http.defaults.headers.post["Content-Type"] = "application/json";
